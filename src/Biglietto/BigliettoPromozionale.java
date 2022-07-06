beae055cd464d02244bfdfc9d0d7e063d1e27da0
christiangreco01 <c.greco13@campus.uniurb.it>
2022-07-06 22:59:19 +0200

    modifica BigliettoPromozionale

--- src/Biglietto/BigliettoPromozionale.java
+++ src/Biglietto/BigliettoPromozionale.java
@@ -5,18 +5,8 @@ public class BigliettoPromozionale extends AbstractBiglietto{
 	private final double prezzoUomo;
 	private final double prezzoDonna;
 	private final double prezzoBambino;
-	private final int percentualeSconto;
 	
-	public BigliettoPromozionale(double prezzoUomo, double prezzoDonna, double prezzoBambino, int percentualeSconto) {
-		this.percentualeSconto = percentualeSconto;
-		super((prezzoUomo / 100) * this.percentualeBiglietto()),
-			  (prezzoDonna / 100) * this.percentualeBiglietto()),
-			  (prezzoBambino / 100) * this.percentualeBiglietto());
+	public BigliettoPromozionale(double prezzoUomo, double prezzoDonna, double prezzoBambino) {
+		super(prezzoUomo,prezzoDonna,prezzoBambino);
 	}
-	
-	//metodo privato che calcola quanta percentuale avrà il biglietto con sconto applicato
-	private int percentualeBiglietto() {
-		return (100 - this.percentualeSconto);
-	}
-
 }
