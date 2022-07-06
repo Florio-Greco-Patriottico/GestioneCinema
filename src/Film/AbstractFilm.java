751f5bbbdb65f04921a5c9fe307c69731511db16
Patriottic0 <a.patriottico1@campus.uniurb.it>
2022-07-06 18:43:05 +0200

    Creazione classe AbstractFilm
    Creazione di una classe astratta che implementa l'interfaccia IFilm.

--- src/Film/AbstractFilm.java
+++ src/Film/AbstractFilm.java
@@ -1,12 +1,53 @@
 package Film;
 
 public class AbstractFilm implements IFilm{
-
+	
+	private String[] name;
+	private String[] gender;
+	private String[] plot;
+	private boolean movieType;
+	private boolean entranceType;
+	
+	public AbstractFilm(String[] name, String[] gender,String[] plot, boolean movieType, boolean entranceType) {
+		this.name = name;
+		this.gender = gender;
+		this.plot = plot;
+		this.movieType = movieType;
+		this.entranceType = entranceType;
+	}
+	
 	@Override
 	public String[] getNome() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
-
+	@Override
+	public String[] getGender() {
+		return this.gender;
+	}
+	
+	@Override
+	public String[] getPlot() {
+		return this.plot;
+	}
+	
+	@Override
+	public boolean getMovieType() {
+		return this.movieType;
+	}
+	
+	@Override
+	public boolean getEntranceType() {
+		return this.entranceType;
+	}
+	
+	@Override
+	public void setPlot (String[] plot) {
+		this.plot = plot;
+	}
+	
+	@Override
+	public void setMovieType(boolean movieType){
+		this.movieType = movieType;
+	}
 }
