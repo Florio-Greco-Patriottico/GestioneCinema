4db7ab8c779f685c5295cc980c29538321ca7395
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-07-12 12:18:27 +0200

    Implements Bar class

--- src/Bar/Ibar.java
+++ src/Bar/Ibar.java
@@ -1,8 +1,21 @@
 package Bar;
 
+import java.util.List;
+
+import Product.AbstractProduct;
+import Utente.AbstractUtente;
+
 public interface Ibar {
-
-
 	
-	
+	//metodo per ritoranare il nome
+	public String getName();
+	//metodo per ritornare i prodotti
+	public List<AbstractProduct> getProduct();
+	//metodo per ritornare i commessi
+	public List<AbstractUtente>  getShopAssistant();
+	//metodo per aggiungere un prodotto
+	public void addProduct(AbstractProduct p);
+	//metodo per aggiungere un commesso
+	public void addShopAssistant(AbstractUtente u);
+
 }//end Ibar
---
+++ src/Utente/Commesso.java
@@ -0,0 +1,11 @@
+package Utente;
+
+public class Commesso extends AbstractUtente{
+
+	//costruttore
+	public Commesso(int age, String name, String surname, boolean gender) {
+		super(age, name, surname, gender);
+		// TODO Auto-generated constructor stub
+	}//end costruttore
+
+}//end commesso
