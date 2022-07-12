2e9ffd735e361527673fb7c2767567dd63195cf9
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-07-12 12:26:33 +0200

    Create TestBar.java

---
+++ src/Test/TestBar.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class TestBar {
+	
+	//dichairazione degli attributi funzionali
+	
+	
+	public void createData() {
+		
+	}//end createData()
+	
+	@org.junit.Test
+	public void testBar() {
+		
+	}//end testBar
+	
+}//end testBar
