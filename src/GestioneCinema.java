c80b971da873bc429672cba044b7ffb8e8ee3ef7
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-08-19 12:11:41 +0200

    update main

--- src/GestioneCinema.java
+++ src/GestioneCinema.java
@@ -1,6 +1,8 @@
 import Bar.Bar;
 import Film.AbstractFilm;
 import Film.Film;
+import Product.Beverage;
+import Product.Snack;
 import Utente.Commesso;
 import Utente.Gestore;
 
@@ -10,14 +12,18 @@ public class GestioneCinema {
 	{  
 		//inizializzo un Gestore
 		Gestore gestore = new Gestore(0, null, null, false);
-		
-		
 		//iniazializzo il bar
 		Bar bar 		= new Bar(null);
 		//inizializzo un Commesso
 		Commesso commesso = new Commesso(0, null, null, false);
 		//aggiungo il commesso al bar
-		bar.addShopAssistant(commesso);
+		bar.addShopAssistant(commesso); 
+		//inizializzo dei prodotti per il bar
+		Beverage cocacola = new Beverage();
+		Snack snack	  = new Snack();
+		//aggiungo i prodotti al bar
+		bar.addProduct(snack);
+		bar.addProduct(cocacola);
 		
 	}//end main	  
 }//end gestioneCinema
