2eea530ae6e0f2b832c29ac7f88b20aae362273f
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-08-24 18:05:54 +0200

    update

--- src/Film/AbstractFilm.java
+++ src/Film/AbstractFilm.java
@@ -1,7 +1,12 @@
 package Film;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class AbstractFilm implements IFilm{
 	
+
+
 	private String[] name;
 	private String[] gender;
 	private String[] plot;
@@ -49,5 +54,35 @@ public class AbstractFilm implements IFilm{
 	@Override
 	public void setMovieType(boolean movieType){
 		this.movieType = movieType;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(gender);
+		result = prime * result + Arrays.hashCode(name);
+		result = prime * result + Arrays.hashCode(plot);
+		result = prime * result + Objects.hash(entranceType, movieType);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractFilm other = (AbstractFilm) obj;
+		return entranceType == other.entranceType && Arrays.equals(gender, other.gender) && movieType == other.movieType
+				&& Arrays.equals(name, other.name) && Arrays.equals(plot, other.plot);
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractFilm [name=" + Arrays.toString(name) + ", gender=" + Arrays.toString(gender) + ", plot="
+				+ Arrays.toString(plot) + ", movieType=" + movieType + ", entranceType=" + entranceType + "]";
 	}
 }
--- src/Utente/Commesso.java
+++ src/Utente/Commesso.java
@@ -6,6 +6,5 @@ public class Commesso extends AbstractUtente{
 	public Commesso(int age, String name, String surname, boolean gender) {
 		super(age, name, surname, gender);
 		// TODO Auto-generated constructor stub
-	}//end costruttore
-
+	}
 }//end commesso
