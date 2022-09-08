e662c6996829ca0a1d963d60df6da16f505b5bac
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-09-08 20:42:58 +0200

    update TestFilm

--- src/Test/TestFilm.java
+++ src/Test/TestFilm.java
@@ -1,17 +1,35 @@
 package Test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import Film.AbstractFilm;
+import Film.Film;
+
 public class TestFilm {
 	
 	//dichairazione degli attributi funzionali
-	
+	private Random rnd;
+	private AbstractFilm film_1;
+	private AbstractFilm film_2;
+
 	
 	public void createData() {
-		
+		this.rnd = new Random();
+		film_1   = new Film("Terminator", "avventura", null, true, true);
+		film_2   = new Film("Spider-Man", "azione", null, false, false);
 	}//end createData()
 	
 	@org.junit.Test
 	public void testFilm() {
-		
+		assertEquals(film_1.equals(film_2),false);
+		assertEquals(film_2.equals(film_1),false);
+		assertEquals(film_1.getName().equals("Terminator"),true);
+		assertEquals(film_2.getName().equals("Terminator"),false);
+		assertEquals(film_2.getName().equals("Spider-Man"),true);
+		assertEquals(film_1.getMovieType(),true);
+		assertEquals(film_2.getMovieType(),false);
 	}//end testFilm
 	
 }//end TestFilm
