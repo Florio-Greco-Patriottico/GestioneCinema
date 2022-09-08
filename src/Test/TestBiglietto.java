ae3d39e4242fed1a5f93a47f8c692e3342c9664e
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-09-08 20:44:59 +0200

    update TestBiglietto

--- src/Test/TestBiglietto.java
+++ src/Test/TestBiglietto.java
@@ -1,17 +1,34 @@
 package Test;
 
+import java.util.Random;
+
+import Biglietto.AbstractBiglietto;
+import Biglietto.BigliettoOrdinario;
+import Biglietto.BigliettoPromozionale;
+
 public class TestBiglietto {
 
 	//dichairazione degli attributi funzionali
+	private Random rnd;
+	private AbstractBiglietto biglietto_1;
+	private AbstractBiglietto biglietto_2;
+	
 	
 	
 	public void createData() {
-		
+		this.rnd = new Random();
+		this.biglietto_1 = new BigliettoOrdinario(0, 0, 0);
+		this.biglietto_2 = new BigliettoPromozionale(0, 0, 0);
 	}//end createData()
 	
 	@org.junit.Test
-	public void testBiglietto() {
+	public void testBigliettoOrdinario() {
 		
-	}//end testBiglietto
+	}//end testBiglietto Ordinario
+	
+	@org.junit.Test
+	public void testBigliettoPromozionale() {
+		
+	}//end testBiglietto Promozionale
 	
 }//end testBiglietto
