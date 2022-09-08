1779db7ed6717116746d10d3ae67708aed19c254
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-09-08 20:48:03 +0200

    update TestBiglietto

--- src/Test/TestBiglietto.java
+++ src/Test/TestBiglietto.java
@@ -1,5 +1,7 @@
 package Test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Random;
 
 import Biglietto.AbstractBiglietto;
@@ -12,10 +14,9 @@ public class TestBiglietto {
 	private Random rnd;
 	private AbstractBiglietto biglietto_1;
 	private AbstractBiglietto biglietto_2;
-	
-	
-	
+
 	public void createData() {
+		//inizializzazione degli attributi funzionali
 		this.rnd = new Random();
 		this.biglietto_1 = new BigliettoOrdinario(0, 0, 0);
 		this.biglietto_2 = new BigliettoPromozionale(0, 0, 0);
@@ -23,12 +24,18 @@ public class TestBiglietto {
 	
 	@org.junit.Test
 	public void testBigliettoOrdinario() {
-		
+		assertEquals(biglietto_1.equals(biglietto_2),false);
+		assertEquals(biglietto_1.getManPrice() == 20.00,true);
+		assertEquals(biglietto_1.getChildPrice() == 8.00,true);
+		assertEquals(biglietto_1.getWomanPrice() == 18.00,true);
 	}//end testBiglietto Ordinario
 	
 	@org.junit.Test
 	public void testBigliettoPromozionale() {
-		
+		assertEquals(biglietto_2.equals(biglietto_1),false);
+		assertEquals(biglietto_1.getManPrice() == 20.00,true);
+		assertEquals(biglietto_1.getChildPrice() == 8.00,true);
+		assertEquals(biglietto_1.getWomanPrice() == 18.00,true);
 	}//end testBiglietto Promozionale
 	
 }//end testBiglietto
