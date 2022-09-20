125c9dd230aa21714bb9240a39b8f4d3a2f58af9
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-09-20 16:17:51 +0200

    update and create Manager

---
+++ src/Controller/ManageCinema.java
@@ -0,0 +1,10 @@
+package Controller;
+
+public class ManageCinema implements Manager {
+	/*
+	 * implementazione metodi interfaccia (Manager)
+	 * 
+	 * 
+	 * 
+	 */
+}//end ManageCinema
---
+++ src/Controller/Manager.java
@@ -0,0 +1,10 @@
+package Controller;
+
+public interface Manager {
+	/*
+	 * 1. public void CreateSale();
+	 * 2. public void CreateProduct()
+	 * 3. public void CreateShopAssitants();
+	 * 4. public void AddProductToBar();
+	 */
+}//end Manager
