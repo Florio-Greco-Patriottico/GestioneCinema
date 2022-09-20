9dabc0c46cb035507e2ce072a93774870b6cf5e4
christiangreco01 <c.greco13@campus.uniurb.it>
2022-09-20 18:18:17 +0200

    uptade

--- src/Controller/ManageCinema.java
+++ src/Controller/ManageCinema.java
@@ -1,5 +1,20 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import Product.Beverage;
+import Product.IProduct;
+import Product.Snack;
+import Sala.ISala;
+import Sala.Sala2D;
+import Sala.Sala3D;
+
 public class ManageCinema implements Manager {
 	/*
 	 * implementazione metodi interfaccia (Manager)
@@ -7,4 +22,59 @@ public class ManageCinema implements Manager {
 	 * 
 	 * 
 	 */
+	
+	private final Set<ISala> sale;
+	private final Set<IProduct> products;
+	
+	public ManageCinema(Set<ISala> sale, Set<IProduct> products){
+		this.sale = sale;
+		this.products = products;
+	}
+	
+	public Set<ISala> getSale() {
+		return sale;
+	}
+	
+	public Set<IProduct> getProducts() {
+		return products;
+	}
+
+	/**
+	 * Creates a list of sale.
+	 * 
+	 * @return a list of sale
+	 */
+	public Set<ISala> createSale() {
+		Set<ISala> sale = new HashSet<>();
+		
+		//creo una lista di sale
+		List<ISala> SaleCinema = Arrays.asList(new Sala2D("sala 1",1,30,true),
+											   new Sala3D("sala 2",2,20,false),
+											   new Sala2D("sala 3",3,40,true));
+		
+		//aggiungo tutte le sale create all'insieme delle sale
+		sale.addAll(SaleCinema);
+		
+		//restituisco le sale create
+		return sale;
+	}		
+	
+	/**
+	 * Creates a list of product.
+	 * 
+	 * @return a list of product
+	 */
+	public Set<IProduct> createProduct() {
+		Set<IProduct> products = new HashSet<>();
+		
+		//creo una lista di sale
+		List<IProduct> AllProducts = Arrays.asList(new Snack(),
+												   new Beverage());
+		
+		//aggiungo tutte le sale create all'insieme delle sale
+		products.addAll(AllProducts);
+		
+		//restituisco le sale create
+		return products;
+	}		
 }//end ManageCinema
--- src/Controller/Manager.java
+++ src/Controller/Manager.java
@@ -1,10 +1,31 @@
 package Controller;
 
+import java.util.Set;
+
+import Product.IProduct;
+import Sala.ISala;
+
 public interface Manager {
 	/*
-	 * 1. public void CreateSale();
+	 * 1. public Set<ISala> CreateSale();
 	 * 2. public void CreateProduct()
 	 * 3. public void CreateShopAssitants();
 	 * 4. public void AddProductToBar();
 	 */
+	
+	/**
+	 * @return a Set of all the products
+	 */
+	Set<IProduct> getProducts();
+	
+	/**
+	 * @return a Set of all the Sale
+	 */
+	Set<ISala> getSale();
+	
+	/**
+	 * create all the Sale
+	 */
+	Set<ISala> createSale();
+	
 }//end Manager
