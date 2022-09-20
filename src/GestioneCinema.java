756bde79aa02fbe0bc280cb3be4dea3eb00ed046
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2022-09-20 16:04:27 +0200

    update JFrame

--- src/GestioneCinema.java
+++ src/GestioneCinema.java
@@ -3,8 +3,9 @@ public class GestioneCinema {
 	
 	public static void main(String args[])
 	{	
-		
+		//inizializzo il JFrameM
 		JframeM frameM = new JframeM();
+		//avvio il JFrameM
 		frameM.start();
 
 	}//end main	  
